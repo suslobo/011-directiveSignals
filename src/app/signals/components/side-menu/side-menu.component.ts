0d146f08af31dffd1af4bc5a18b4fe08868c4fc4
slobo <suslobo@hotmail.es>
2024-09-09 16:19:50 +0200

    Menú lateral

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+//creamos una interface
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: 'side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent {
+
+  //creamos una propiedad
+  public menuItems: MenuItem[] = [
+    //vamos a inicializarlo
+    { title: 'Contador', route: 'counter' },
+    { title: 'Usuario', route: 'user-info' },
+    { title: 'Mutaciones', route: 'properties' },
+  ];
+
+
+}
--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -6,16 +6,16 @@ import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.c
 import { CounterPageComponent } from './pages/counter-page/counter-page.component';
 import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
-
-  
     SignalsLayoutComponent,
-         CounterPageComponent,
-         UserInfoPageComponent,
-         PropertiesPageComponent
+    CounterPageComponent,
+    UserInfoPageComponent,
+    PropertiesPageComponent,
+    SideMenuComponent
   ],
   imports: [
     CommonModule,
