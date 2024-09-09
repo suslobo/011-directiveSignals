9823341574bf5c532c45f9285003b9e27959cd5b
slobo <suslobo@hotmail.es>
2024-09-09 12:16:15 +0200

    Formulario reactivo tradicional

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css'
 })
 export class ProductPageComponent {
+
+  //antes lo hacíamos así y se puede hacer
+  // constructor( private fb: FormBuilder) {}
+
+  private fb = inject ( FormBuilder );
+  //creamos una propiedad llamada color
+  public color:string= 'green';
+  //creamos nuestro formulario
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(6), Validators.email] ] //el campo name tiene tres validaciones
+  });
+
+  //creamos nuevo método
+  changeColor() {
+    this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+  }
 
 }
