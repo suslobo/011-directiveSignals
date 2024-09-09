7d9f8cf3cb9fdc15b7df2dd0ae7780f3fa84c700
slobo <suslobo@hotmail.es>
2024-09-09 14:01:07 +0200

    Cambiar el mensaje de la etiqueta

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 //las directivas deben ser declaradas en el modulo shared.module.ts
 //como los componentes
@@ -12,14 +13,23 @@ export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
   //podemos crearno una referencia para saber cual es el color de mi directiva
   private _color:string = 'red';
+  //creamos otra popiedad
+  private _errors?: ValidationErrors | null;
 
   //queremos recibir el color
   @Input() set color( value:string ) {
     this._color = value;
     //cada vez que el color o el estilo cambie hago llamar el setStyle
     this.setStyle();
+  }
+
+  //hacemos el @Input para poder recibir ValidationErrors y lo conectamos en el html line21
+  @Input() set errors( value: ValidationErrors | null | undefined ) {
+    this._errors = value;
+    this.setErrorMessage();
 
   }
+
 
   constructor( private el: ElementRef<HTMLElement>) {
     // console.log('constructor de la directiva')
@@ -37,6 +47,42 @@ export class CustomLabelDirective implements OnInit {
     if ( !this.htmlElement ) return;
 
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  //nuevo método
+  //lo llamamos en su input @Input() set errors
+  setErrorMessage():void {
+    if (! this.htmlElement ) return; //si no tenemos el elemento html no hacemos nada
+    if ( !this._errors) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }  //si esto es null, limpia los errores
+
+    //posibles errores
+    //nos creamos una array de los errores
+    const errors = Object.keys(this._errors);
+
+    //si el array de errores incluye el required
+    if ( errors.includes('required') ) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido.';
+      return; //para que no siga haciendo nada más
+    }
+
+    if ( errors.includes('minLength')) {
+      const min = this._errors!['minlength'] ['requiredLength'];
+      const current = this._errors!['minlength'] ['actualLength'];
+      this.htmlElement.nativeElement.innerHTML = `Mínimo ${current}/${min} caracteres.`;
+      return;
+    }
+
+    if ( errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText = 'No tiene formato de correo.';
+      return;
+    }
+
+
+
+
 
   }
 
