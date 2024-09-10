c51bdadd77de7fe6422392897d7a1595272ba639
slobo <suslobo@hotmail.es>
2024-09-10 12:29:45 +0200

    Counter Signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
 
+  public counter = signal(10);
+  //squareCounter va a estar siempre pendiente de la señal this.counter
+  //o de todas dentro de este método computado
+  //es una propiedad solo de lectura
+  public squareCounter = computed( () => this.counter() * this.counter() );
+  // creamos un metodo increase para que el botón incremente en 1 en el html
+  increaseBy( value: number) {
+    //el valor actual más el valor
+    this.counter.update( current => current + value )
+  }
 }
