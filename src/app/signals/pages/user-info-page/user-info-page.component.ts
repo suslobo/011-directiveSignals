3973f9784a5f912f0f8ce0d9d4b0560e932e76da
slobo <suslobo@hotmail.es>
2024-09-10 15:34:48 +0200

    Manejo de errores y propiedades computadas

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { UsersServiceService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -8,13 +8,21 @@ import { User } from '../../interfaces/user-request.interface';
   styleUrl: './user-info-page.component.css'
 })
 export class UserInfoPageComponent implements OnInit {
-
+  //creamos las propiedades
   //hacermos la inyección de nuestro servicio usersService
   private usersService = inject(UsersServiceService);
   public userId = signal(1);   //usuario inicial
 
   public currentUser = signal<User | undefined>(undefined);  //usuario actual
   public userWasFound = signal(true);
+
+  public fullName = computed<string>( () => { //nombre
+    //si no tenemos ese usuario
+    if ( !this.currentUser() ) return 'Usuario no encontrado';
+    //si tenemos un usuario y no es un valor undefined
+    return `${ this.currentUser()?.first_name } ${ this.currentUser()?.last_name }`;
+
+  });
 
   ngOnInit(): void {
     this.loadUser( this.userId() ) //este es el valor de la señal
@@ -25,11 +33,19 @@ export class UserInfoPageComponent implements OnInit {
     if ( id <= 0 ) return;
     //si es un valor mayor a 0
     this.userId.set(id); //sincrono
+    this.currentUser.set(undefined);
     //ahora hacemos la petición http
     this.usersService.getUserById( id )
-      .subscribe( user => {
-        this.currentUser.set( user )
-      })
+      .subscribe({ //cuando todo sale bien
+        next: (user) => {
+          this.currentUser.set( user );
+          this.userWasFound.set(true);
+        },
+        error: () => {  //si sucede un error
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        },
+    });
 
 
 
